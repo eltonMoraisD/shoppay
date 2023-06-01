c8f9a58d66baab5503103b2d7b393e537e91eb4d
Elton <admin@Admins-MBP.fritz.box>
2023-06-01 16:34:59 +0200

    create User menu

--- components/header/Top.js
+++ components/header/Top.js
@@ -3,40 +3,64 @@ import { MdSecurity } from "react-icons/md";
 import { BsSuitHeart } from "react-icons/bs";
 import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
 import Link from "next/link";
+import { useState } from "react";
+import UserMenu from "./UserMenu";
 
 export default function Top() {
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [visible, setVisible] = useState(false);
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
         <div></div>
         <ul className={styles.top__list}>
-          <li>
+          <li className={styles.li}>
             <img
               src="https://flagdownload.com/wp-content/uploads/Flag_of_Luxembourg_Flat_Round-512x512.png"
               alt="flag"
             />
             <span>Luxembourg / eur</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <MdSecurity />
             <span>Buyer Protection</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Customer Service</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Help</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <BsSuitHeart />
             <Link href="/profile/whishlist">Whishlist</Link>
           </li>
-          <li>
-            <div className={styles.flex}>
-              <RiAccountPinCircleLine />
-              <span>Account</span>
-              <RiArrowDropDownFill />
-            </div>
+          <li
+            className={styles.li}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
+            {loggedIn ? (
+              <li>
+                <div className={styles.flex}>
+                  <img
+                    src="https://avatars.githubusercontent.com/u/39710764?v=4"
+                    alt=""
+                  />
+                  <span>Elton</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            {visible && <UserMenu loggedIn={loggedIn} />}
           </li>
         </ul>
       </div>
---
+++ components/header/UserMenu.js
@@ -0,0 +1,46 @@
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+export default function UserMenu({ loggedIn }) {
+  return (
+    <div className={styles.menu}>
+      <h4>Welcome to Shoppay !</h4>
+      {loggedIn ? (
+        <div className={styles.flex}>
+          <img
+            src="https://avatars.githubusercontent.com/u/39710764?v=4"
+            alt=""
+            className={styles.menu__img}
+          />
+          <div className={styles.col}>
+            <span>Welcome Back,</span>
+            <h3>Elton</h3>
+            <span>Sign Out</span>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.flex}>
+          <button className={styles.btn_primary}>Register</button>
+          <button className={styles.btn_outlined}>Login</button>
+        </div>
+      )}
+      <ul>
+        <li>
+          <Link href="/profile">Account</Link>
+        </li>
+        <li>
+          <Link href="/profile/orders">My Orders</Link>
+        </li>
+        <li>
+          <Link href="/profile/messages">Message Center</Link>
+        </li>
+        <li>
+          <Link href="/profile/address">Address</Link>
+        </li>
+        <li>
+          <Link href="/profile/whishlist">Whishlist</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
