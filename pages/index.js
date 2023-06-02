67a6a7ad40695f9f2b3ce68ca1d86315df331019
Elton <admin@Admins-MBP.fritz.box>
2023-06-02 23:14:07 +0200

    style footer links

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Footer from "../components/footer";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
