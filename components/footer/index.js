9e981e8f861857db68bb6b6ddc12f7733e72d247
Elton <admin@Admins-MBP.fritz.box>
2023-06-05 14:16:50 +0200

    style footer newsletter and social links

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,11 +1,15 @@
 import styles from "../footer/styles.module.scss";
 import Links from "./Links";
+import Newsletter from "./Newsletter";
+import Socials from "./Socials";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
+        <Socials />
+        <Newsletter />
       </div>
     </footer>
   );
